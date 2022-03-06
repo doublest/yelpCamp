325ec6865c8c15298be6b5dfd447dc9777804f15
Stefan Stieber <s.stieber@me.com>
2022-03-06 16:59:56 +0100

    edit & update

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 const path = require('path')
 const mongoose = require('mongoose');
 const Campground = require('./models/campground');
+const methodOverride = require('method-override');
+const {response} = require("express");
 const port = 3000;
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
@@ -21,6 +23,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
 
 app.get('/', (req, res) => {
     res.render('home');
@@ -47,6 +50,17 @@ app.get('/campgrounds/:id', async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     res.render('campgrounds/show', { campground });
 });
+
+app.get('/campgrounds/:id/edit', async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/edit', { campground });
+});
+
+app.put('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground});
+    res.redirect(`/campgrounds/${campground._id}`)
+})
 
 app.listen(port, () => {
     console.log(`YelCamp Server hast started on PORT: ${port}`);
