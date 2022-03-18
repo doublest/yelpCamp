019bc80e54875f5ad09efbb918f724a73424c5e4
Stefan Stieber <s.stieber@me.com>
2022-03-18 22:04:49 +0100

    add new immage loop, source unsplash

--- seeds/index.js
+++ seeds/index.js
@@ -23,9 +23,13 @@ const seedDB = async () => {
     for (let i = 0; i < 50; i++) {
         //there are 1000 cities in the array
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) +10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'rem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed d',
+            price
         })
         await camp.save();
     }
