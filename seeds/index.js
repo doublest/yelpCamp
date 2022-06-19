a0224236898d1813a2d4760ead1440e7d3fa734f
Stefan Stieber <s.stieber@me.com>
2022-06-19 18:05:40 +0200

    fixing the seeds db

--- seeds/index.js
+++ seeds/index.js
@@ -28,9 +28,18 @@ const seedDB = async () => {
             author: '62966ea07c21fd20203be06b',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
-            image: 'https://source.unsplash.com/collection/483251',
             description: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed d',
-            price
+            price,
+            images: [
+                {
+                    url: 'https://res.cloudinary.com/dgf4mahia/image/upload/v1655652854/YelpCamp/ql3aapbsdvbaklprkss0.jpg',
+                    filename: 'YelpCamp/ql3aapbsdvbaklprkss0'
+                },
+                {
+                    url: 'https://res.cloudinary.com/dgf4mahia/image/upload/v1655652855/YelpCamp/f9q0iorx85cd8lata77n.jpg',
+                    filename: 'YelpCamp/f9q0iorx85cd8lata77n'
+                }
+            ]
         })
         await camp.save();
     }
