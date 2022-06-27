9972c31d957901d0ea0c7e46433977e6c82ddd4e
Stefan Stieber <s.stieber@me.com>
2022-06-27 21:24:53 +0200

    update edit form

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -45,6 +45,7 @@ module.exports.updateCampground = async (req, res) => {
     const campground = await Campground.findByIdAndUpdate(id, {...req.user.campground});
     const imgs = req.files.map(f => ({url: f.path, filename: f.filename}));
     campground.images.push(...imgs);
+
     await campground.save();
     req.flash('success', 'Successfully updated campground');
     res.redirect(`/campgrounds/${campground._id}`)
