a29cd4562a2c6c0b7072f455e1f578a8ce36805c
Stefan Stieber <s.stieber@me.com>
2022-07-03 20:55:32 +0200

    add map feature

--- models/campground.js
+++ models/campground.js
@@ -16,6 +16,17 @@ const CampgroundSchema = new Schema({
     title: String,
     price: Number,
     images: [ImageSchema],
+    geometry: {
+      type: {
+          type:String,
+          enum: ['Point'],
+          required: true
+      },
+      coordinates: {
+          type: [Number],
+          required: true
+      }
+    },
     description: String,
     location: String,
     author: {
